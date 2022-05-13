c10119863402ae61624b93f90a61ec8141c6ac2b
Richard Noonan <noonanri@tcd.ie>
2022-05-13 07:56:04 +0100

    added pay withdraw functions

--- contracts/SimpleContractAgreement.sol
+++ contracts/SimpleContractAgreement.sol
@@ -5,34 +5,44 @@ contract SimpleContractAgreement {
     address employer;
     address employee;
     uint256 maxEmployees = 1;
-    uint256 contractMonths;
-    uint256 employerCounter;
-    uint256 employeeCounter;
-    int256 paymentAmount;
+    uint256 startDate = 0;
+    uint256 endDate = 0;
+    uint256 employerCounter = 0;
+    uint256 employeeCounter = 0;
+    uint256 paymentAmount = 0;
+    bool employerDispute = false;
+    event Received(address, uint256);
 
-    function setContractMonths(uint256 months) public onlyEmployer {
-        contractMonths = months;
+    function setStartEndDate(uint256 start, uint256 end) public onlyEmployer {
+        require(startDate < endDate);
+        startDate = start;
+        endDate = end;
     }
 
-    function getContractMonths() public view returns (uint256) {
-        return contractMonths;
+    function withdrawPayment() public payable onlyEmployee {
+        require(
+            endDate != 0 &&
+                block.timestamp >= endDate &&
+                employerDispute == false
+        );
+        address payable receiver = payable(msg.sender);
+        receiver.transfer(paymentAmount);
     }
 
-    function setPaymentAmount(int256 amount) public onlyEmployer {
-        if (paymentAmount == 0) {
-            paymentAmount = amount;
-        }
+    receive() external payable onlyEmployer {
+        require(paymentAmount == 0 && block.timestamp < startDate);
+        paymentAmount = msg.value;
+        emit Received(msg.sender, msg.value);
     }
 
-    function getPaymentAmount() public view returns (int256) {
+    function getPaymentAmountUSD() public view returns (uint256) {
         return paymentAmount;
     }
 
     function setEmployer() public notEmployee {
-        if (employerCounter < 1) {
-            employer = msg.sender;
-            employerCounter++;
-        }
+        require(employerCounter < 1);
+        employer = msg.sender;
+        employerCounter++;
     }
 
     function getEmployer() public view returns (address) {
@@ -40,10 +50,9 @@ contract SimpleContractAgreement {
     }
 
     function setEmployee() public notEmployer {
-        if (employeeCounter < 1) {
-            employee = msg.sender;
-            employeeCounter++;
-        }
+        require(employerCounter > 0 && employeeCounter < 1);
+        employee = msg.sender;
+        employeeCounter++;
     }
 
     function getEmployee() public view returns (address) {
@@ -54,6 +63,14 @@ contract SimpleContractAgreement {
         require(
             msg.sender == employer,
             "Only Employer allowed to call this function"
+        );
+        _;
+    }
+
+    modifier onlyEmployee() {
+        require(
+            msg.sender == employee,
+            "Only Employee allowed to call this function"
         );
         _;
     }
