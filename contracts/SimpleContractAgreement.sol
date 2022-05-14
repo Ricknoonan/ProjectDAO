d4799c35b90f49d56eb6f5b9a771e4fcb5f88614
Richard Noonan <noonanri@tcd.ie>
2022-05-14 09:10:07 +0100

    withdraw, add particapant fucntionalit and also refactored a lot of code

--- contracts/SimpleContractAgreement.sol
+++ contracts/SimpleContractAgreement.sol
@@ -7,11 +7,33 @@ contract SimpleContractAgreement {
     uint256 maxEmployees = 1;
     uint256 startDate = 0;
     uint256 endDate = 0;
-    uint256 employerCounter = 0;
     uint256 employeeCounter = 0;
     uint256 paymentAmount = 0;
     bool employerDispute = false;
+    uint256 stakeAmount = 0;
+    uint256 contrtMonths = 0;
     event Received(address, uint256);
+
+    constructor(
+        uint256 _paymentAmount,
+        uint256 _stakePercent,
+        uint256 _contractMonths
+    ) {
+        employer = msg.sender;
+        paymentAmount = _paymentAmount;
+        stakeAmount = (_stakePercent / 100) * paymentAmount;
+        contrtMonths = _contractMonths;
+    }
+
+    struct particpant {
+        string particpantType;
+        bool isStake;
+        address particpantAddr;
+        bool isEmployer;
+        uint256 stakeAmount;
+    }
+
+    mapping(address => particpant) public particpants;
 
     function setStartEndDate(uint256 start, uint256 end) public onlyEmployer {
         require(startDate < endDate);
@@ -19,40 +41,80 @@ contract SimpleContractAgreement {
         endDate = end;
     }
 
-    function withdrawPayment() public payable onlyEmployee {
+    // withdraw function allows particpants to withdraw payment and stake for Employees
+    // and stake for employers
+    function withdraw(string memory _type)
+        public
+        payable
+        particpantTypeAllowed(_type)
+    {
         require(
             endDate != 0 &&
                 block.timestamp >= endDate &&
                 employerDispute == false
         );
-        address payable receiver = payable(msg.sender);
-        receiver.transfer(paymentAmount);
-    }
-
-    receive() external payable onlyEmployer {
-        require(paymentAmount == 0 && block.timestamp < startDate);
-        paymentAmount = msg.value;
-        emit Received(msg.sender, msg.value);
+        if (
+            keccak256(abi.encodePacked((_type))) ==
+            keccak256(abi.encodePacked(("Employer")))
+        ) {
+            address payable receiver = payable(msg.sender);
+            uint256 stake = particpants[msg.sender].stakeAmount;
+            receiver.transfer(stake);
+        }
+        if (
+            keccak256(abi.encodePacked((_type))) ==
+            keccak256(abi.encodePacked(("Employee")))
+        ) {
+            address payable receiver = payable(msg.sender);
+            uint256 stake = particpants[msg.sender].stakeAmount;
+            receiver.transfer(stake + paymentAmount);
+        }
     }
 
     function getPaymentAmountUSD() public view returns (uint256) {
         return paymentAmount;
     }
 
-    function setEmployer() public notEmployee {
-        require(employerCounter < 1);
-        employer = msg.sender;
-        employerCounter++;
-    }
-
     function getEmployer() public view returns (address) {
         return employer;
     }
 
-    function setEmployee() public notEmployer {
-        require(employerCounter > 0 && employeeCounter < 1);
-        employee = msg.sender;
-        employeeCounter++;
+    function setParticpant(string memory _type)
+        public
+        payable
+        particpantTypeAllowed(_type)
+    {
+        if (
+            keccak256(abi.encodePacked((_type))) ==
+            keccak256(abi.encodePacked(("Employer")))
+        ) {
+            require(block.timestamp < startDate);
+            require(
+                msg.value > (stakeAmount + paymentAmount),
+                "Insufficent amount"
+            );
+            emit Received(msg.sender, msg.value);
+            particpants[msg.sender].particpantType = _type;
+            particpants[msg.sender].isStake = true;
+            particpants[msg.sender].particpantAddr = msg.sender;
+            particpants[msg.sender].isEmployer = true;
+            particpants[msg.sender].stakeAmount = msg.value;
+        }
+        if (
+            keccak256(abi.encodePacked((_type))) ==
+            keccak256(abi.encodePacked(("Employee")))
+        ) {
+            require(block.timestamp < startDate);
+            require(employeeCounter < 1);
+            require(msg.value > stakeAmount, "Insufficent amount");
+            emit Received(msg.sender, msg.value);
+            particpants[msg.sender].particpantType = _type;
+            particpants[msg.sender].isStake = true;
+            particpants[msg.sender].particpantAddr = msg.sender;
+            particpants[msg.sender].isEmployer = false;
+            particpants[msg.sender].stakeAmount = msg.value;
+            employeeCounter += 1;
+        }
     }
 
     function getEmployee() public view returns (address) {
@@ -87,6 +149,16 @@ contract SimpleContractAgreement {
         require(
             msg.sender != employee,
             "Employee not allowed to call this function"
+        );
+        _;
+    }
+
+    modifier particpantTypeAllowed(string memory _type) {
+        require(
+            keccak256(abi.encodePacked((_type))) ==
+                keccak256(abi.encodePacked(("Employee"))) ||
+                keccak256(abi.encodePacked((_type))) ==
+                keccak256(abi.encodePacked(("Employer")))
         );
         _;
     }
