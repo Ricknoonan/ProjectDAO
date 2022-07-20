bd713f45f3f09ae2af65e73d4f90d4078b848767
Richard Noonan <noonanri@tcd.ie>
2022-07-20 18:27:42 +0100

    perp contract

---
+++ contracts/SinglePaymentNoEndDate.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <8.10.0;
+
+contract SinglePaymentNoEndDate{
+
+// payment is created
+// employee starts work
+// employee delivers service 
+// employee submits a withdrawal request
+// employer accepts or rejects
+// each reject slashes the funded amount in the contract so that payment amount is reduced or the refund amount is reduced if the contract is terminated
+// after three rejects, the contract is terminated
+// emplyee withdraws
+// employer gets money back if they both agree to terminate agreement 
+// emplyee e
+    function fund() {
+        
+    }
+
+    function withdrawRequest() {
+        
+    }
+
+    function withdraw() {
+        
+    }
+
+    function approveWithdraw(){
+
+    }
+    function rejectWithdraw() {
+        
+    }
+
+    function terminate() {
+        
+    }
+
+}
